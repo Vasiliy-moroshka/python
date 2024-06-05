5d6d783f0aa8da3ce1c33f0799d35adf6a2cdcce
Василий <Wh1lepp@gmail.com>
2024-06-05 18:17:32 +0200

    Ended 21-th task

--- sem03.py
+++ sem03.py
@@ -61,14 +61,33 @@
 # Output: [4, 5, 1, 2, 3]
 
 
-data = [int(i) for i in input("Введите числа: ").split()]
+# data = [int(i) for i in input("Введите числа: ").split()]
 
 
-k = int(input("Введите кол-во сдвигов: ")) % len(data)
-for i in range(k):
-    data.insert(0, data.pop(-1))
-#     # [1, 2, 3, 4, 5] k = 2
-#     # data.pop(-1) -> 5
-#     # data.insert(0, 5)
-#     # [5, 1, 2, 3, 4]
-print(data)
+# k = int(input("Введите кол-во сдвигов: ")) % len(data)
+# for i in range(k):
+#     data.insert(0, data.pop(-1))
+# #     # [1, 2, 3, 4, 5] k = 2
+# #     # data.pop(-1) -> 5
+# #     # data.insert(0, 5)
+# #     # [5, 1, 2, 3, 4]
+# print(data)
+
+
+# Задача №21.
+
+# Напишите программу для печати всех уникальных
+# значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+# {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
+# ":" S007 "}]
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, 
+        {"VI": "S005"}, {"VII": "S005"}, {" V ":"S009"}, {" VIII":"S007"}]
+
+result = set()
+for d in data:
+    for key in d:
+        result.add(d[key])
+print(result)
