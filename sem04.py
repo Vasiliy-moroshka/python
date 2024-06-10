3e1f7448bd80ec975b5634ad8eb53dccec2f39dd
Василий <Wh1lepp@gmail.com>
2024-06-10 18:51:54 +0200

    Ended 27-th task

--- sem04.py
+++ sem04.py
@@ -33,15 +33,48 @@
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 
 
-data = input("Введите символы через пробел:" ).split()
-print(data)
+# data = input("Введите символы через пробел:" ).split()
+# print(data)
 
-frequency_dict = {}
-for element in data:
-    if element not in frequency_dict:
-        print(element, end=' ')
-        frequency_dict[element] = 1
-    else:
-        print(f'{element}_{frequency_dict[element]}', end=' ')
-        frequency_dict[element] += 1
+# frequency_dict = {}
+# for element in data:
+#     if element not in frequency_dict:
+#         print(element, end=' ')
+#         frequency_dict[element] = 1
+#     else:
+#         print(f'{element}_{frequency_dict[element]}', end=' ')
+#         frequency_dict[element] += 1
 
+
+# String(строка)
+
+# name = "Petr"
+
+# print(name.lower())
+# print(name.upper())
+
+# print(ord("A"))
+# print(chr(45))
+
+# print([chr(i) for i in range(ord("A"), ord("Z") + 1)])
+
+
+# Задача №27.
+
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+
+# Output: 13
+
+# text = input("Input text: ").lower()
+# separator = "!@?.,;:"
+# for i in separator:
+# 	text = ''.join(text.split(i))
+# print(len(set(text.split())))
